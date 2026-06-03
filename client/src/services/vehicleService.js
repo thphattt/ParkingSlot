f632dbb85c61838340f2ec352f369315ea4c518d
thphattt <buithanhphat433@gmail.com>
2026-06-03 16:19:42 +0700

    feat: add vehicleService for managing vehicle API interactions

---
+++ client/src/services/vehicleService.js
@@ -0,0 +1,11 @@
+import api from "./api";
+
+const vehicleService = {
+   getAll: (params = {}) => api.get('/vehicles', { params }),
+  getById: (id) => api.get(`/vehicles/${id}`),
+  create: (data) => api.post('/vehicles', data),
+  update: (id, data) => api.put(`/vehicles/${id}`, data),
+  delete: (id) => api.delete(`/vehicles/${id}`),
+};
+
+export default vehicleService;
